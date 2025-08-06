f8a4a3c9649e18df66ebf8e9c2765342e54be8e9
3idey <elaidya225@gmail.com>
2025-08-06 19:46:42 +0300

    task 6

---
+++ PHP/Http/Forms/LoginForm.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Http\Forms;
+
+use Core\Validator;
+use Core\ValidationException;
+
+class LoginForm
+{
+    protected $errors = [];
+
+    public function __construct(public array $attributes = [])
+    {
+
+        if (!Validator::email($this->attributes['email'] ?? '')) {
+            $this->errors['email'] = 'Please provide a valid email address.';
+        }
+
+        if (!Validator::string($this->attributes['password'] ?? '', 6)) {
+            $this->errors['password'] = 'Please provide a valid password.';
+        }
+
+        return empty($this->errors);
+    }
+    public function throw()
+    {
+        ValidationException::throw($this->errors(), $this->attributes);
+    }
+
+    public function failed()
+    {
+        return count($this->errors);
+    }
+    public static function validate($attributes)
+    {
+        $instance = new static($attributes);
+
+        return $instance->failed() ? $instance->throw() : $instance;
+    }
+
+    public function errors()
+    {
+        return $this->errors;
+    }
+
+    public function error($field, $message)
+    {
+        $this->errors[$field] = $message;
+        return $this;
+    }
+}
